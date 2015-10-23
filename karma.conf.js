9d5d0fe18684b3e543f9d62decbef077c7e27c63
James York <james@grandcircus.co>
2015-10-22 21:41:28 -0400

    moved the js folder around and updated karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'source/javascript/**/*.js',
+      'javascript/**/*.js',
       'spec/**/*.spec.js'
     ],
 
